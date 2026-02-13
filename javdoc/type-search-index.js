fa54f69b19ebade5511ef93352e7b1f4dacd41b9
da-perry1 <daperry1@ualberta.ca>
2026-02-13 12:47:03 -0700

    Commission

---
+++ javdoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.listycity6","l":"City"},{"p":"com.example.listycity6","l":"CityList"}];updateSearchResults();
